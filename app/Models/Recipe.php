f9eb6ae2d4cf2dbe961ff651cebcc607ea9a151b
rc2pc2 <petriccarcc@gmail.com>
2024-02-08 10:24:42 +0100

    added fillable properties to Recipe resource model

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Recipe extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'image',
+        'complexity',
+        'preparation_time',
+        'cooking_time',
+    ];
 }
