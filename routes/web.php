63318422392f63810e464d02dc5dcae835b3018f
rc2pc2 <petriccarcc@gmail.com>
2024-02-08 10:25:24 +0100

    updated all resources routes with ::resource static method

--- routes/web.php
+++ routes/web.php
@@ -21,26 +21,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () { return view('pages.home');})->name('pages.home');
 Route::get('admin/pastas', [AdminPastaController::class, 'index'])->name('admin.pastas.index');
 
-//
-// Route::resource('pastas', GuestPastaController::class);
+// Route::get('/pastas', [GuestPastaController::class, 'index'])->name('guest.pastas.index');
+// Route::post('/pastas', [GuestPastaController::class, 'store'])->name('guest.pastas.store');
+// Route::get('/pastas/create', [GuestPastaController::class, 'create'])->name('guest.pastas.create');
+// Route::get('/pastas/{pasta}', [GuestPastaController::class, 'show'])->name('guest.pastas.show');
+// Route::put('/pastas/{pasta}', [GuestPastaController::class, 'update'])->name('guest.pastas.update');
+// Route::get('/pastas/{pasta}/edit', [GuestPastaController::class, 'edit'])->name('guest.pastas.edit');
 
-Route::get('/pastas', [GuestPastaController::class, 'index'])->name('guest.pastas.index');
-Route::post('/pastas', [GuestPastaController::class, 'store'])->name('guest.pastas.store');
-Route::get('/pastas/create', [GuestPastaController::class, 'create'])->name('guest.pastas.create');
-Route::get('/pastas/{pasta}', [GuestPastaController::class, 'show'])->name('guest.pastas.show');
-Route::put('/pastas/{pasta}', [GuestPastaController::class, 'update'])->name('guest.pastas.update');
-Route::get('/pastas/{pasta}/edit', [GuestPastaController::class, 'edit'])->name('guest.pastas.edit');
+// Route::get('/mills', [GuestMillController::class, 'index'])->name('guest.mills.index');
+// Route::post('/mills', [GuestMillController::class, 'store'])->name('guest.mills.store');
+// Route::get('/mills/create', [GuestMillController::class, 'create'])->name('guest.mills.create');
+// Route::get('/mills/{mill}', [GuestMillController::class, 'show'])->name('guest.mills.show');
+// Route::get('/mills/{mill}/edit', [GuestMillController::class, 'edit'])->name('guest.mills.edit');
+// Route::put('/mills/{mill}', [GuestMillController::class, 'update'])->name('guest.mills.update');
+
+// Route::get('/recipes', [GuestRecipeController::class, 'index'])->name('guest.recipes.index');
+// Route::post('/recipes', [GuestRecipeController::class, 'store'])->name('guest.recipes.store');
+// Route::get('/recipes/create', [GuestRecipeController::class, 'create'])->name('guest.recipes.create');
+// Route::get('/recipes/{recipe}', [GuestRecipeController::class, 'show'])->name('guest.recipes.show');
+// Route::get('/recipes/{recipe}/edit', [GuestRecipeController::class, 'edit'])->name('guest.recipes.edit');
+// Route::put('/recipes/{recipe}', [GuestRecipeController::class, 'update'])->name('guest.recipes.update');
 
 
-Route::get('/mills', [GuestMillController::class, 'index'])->name('guest.mills.index');
-Route::post('/mills', [GuestMillController::class, 'store'])->name('guest.mills.store');
-Route::get('/mills/create', [GuestMillController::class, 'create'])->name('guest.mills.create');
-Route::get('/mills/{mill}', [GuestMillController::class, 'show'])->name('guest.mills.show');
-Route::get('/mills/{mill}/edit', [GuestMillController::class, 'edit'])->name('guest.mills.edit');
-Route::put('/mills/{mill}', [GuestMillController::class, 'update'])->name('guest.mills.update');
-
-
-Route::get('/recipes', [GuestRecipeController::class, 'index'])->name('guest.recipes.index');
-Route::post('/recipes', [GuestRecipeController::class, 'store'])->name('guest.recipes.store');
-Route::get('/recipes/create', [GuestRecipeController::class, 'create'])->name('guest.recipes.create');
-Route::get('/recipes/{recipe}', [GuestRecipeController::class, 'show'])->name('guest.recipes.show');
+Route::name('guest.')->group(function(){
+    Route::resource('pastas', GuestPastaController::class);
+    // Route::get('/pastas/specials', [GuestPastaController::class, 'specialsIndex'])->name('pastas.specials');
+    Route::resource('mills', GuestMillController::class);
+    Route::resource('recipes', GuestRecipeController::class);
+});
